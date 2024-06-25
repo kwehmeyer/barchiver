c29be640f5149940c5a3870a39df43dacd1c8170
Kahlil Wehmeyer <kahlilwehmeyer@icloud.com>
2024-06-25 12:27:15 -0400

    chore: Update pandas dependency to version 2.2.2

--- src/barchiver/repository.py
+++ src/barchiver/repository.py
@@ -2,12 +2,13 @@
 from spotipy import Spotify
 from .models import LikedSong
 from loguru import logger
+import pandas as pd
 
 
 class UserRepository:
     """User repository for the Barchiver application."""
 
-    liked_tracks: list[LikedSong] = []
+    _liked_tracks: list[LikedSong] = []
 
     def __init__(self, sp: Spotify) -> None:
         """Initialize the user repository."""
@@ -16,10 +17,10 @@ class UserRepository:
     @property
     def get_liked_tracks(self) -> list:
         """Get the liked tracks for the user."""
-        if self.liked_tracks:
-            return self.liked_tracks
+        if self._liked_tracks:
+            return self._liked_tracks
         initial_call = self.sp.current_user_saved_tracks()
-        self.liked_tracks.extend(
+        self._liked_tracks.extend(
             [LikedSong(**track) for track in initial_call.get("items")]
         )
         limit = initial_call.get("total")
@@ -28,7 +29,7 @@ class UserRepository:
         )
         # ? We can probably parallelize this
         for offset in range(20, limit, 20):
-            self.liked_tracks.extend(
+            self._liked_tracks.extend(
                 [
                     LikedSong(**track)
                     for track in self.sp.current_user_saved_tracks(offset=offset).get(
@@ -36,4 +37,22 @@ class UserRepository:
                     )
                 ]
             )
-        return self.liked_tracks
+        return self._liked_tracks
+
+    def get_tracks_month_year(self) -> pd.DataFrame:
+        def agg_track_ids(x) -> list[str]:
+            return x["track"].apply(lambda x: x.get("id")).to_list()
+
+        df = pd.DataFrame([model.model_dump() for model in self.get_liked_tracks])
+        df["added_at"] = pd.to_datetime(df["added_at"])
+        gb = df.groupby(
+            [
+                df["added_at"].dt.year.rename("year"),
+                df["added_at"].dt.month.rename("month"),
+            ]
+        )
+        return (
+            gb.apply(lambda x: agg_track_ids(x))
+            .reset_index()
+            .rename(columns={0: "track_ids"})
+        )
--- tests/test_repository.py
+++ tests/test_repository.py
@@ -1,4 +1,5 @@
 from barchiver.repository import UserRepository
+from pandas import DataFrame
 from barchiver.models import LikedSong
 from conftest import sp
 
@@ -17,3 +18,12 @@ class TestUserRepository:
         assert isinstance(liked, list)
         assert len(liked) > 0
         assert isinstance(liked[0], LikedSong)
+
+    def test_get_tracks_month_year(self):
+        """Test if UserRepository can get tracks grouped by month and year"""
+        user_repo = UserRepository(sp)
+        tracks_month_year = user_repo.get_tracks_month_year()
+        assert isinstance(tracks_month_year, DataFrame)
+        assert "year" in tracks_month_year.columns
+        assert "month" in tracks_month_year.columns
+        assert "track_ids" in tracks_month_year.columns
