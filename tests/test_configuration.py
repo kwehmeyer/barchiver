ce19aff8311810130aff10290d249bd032373e52
Kahlil Wehmeyer <kahlilwehmeyer@icloud.com>
2024-06-25 09:03:40 -0400

    feat: Set Spotify client ID and secret in BArchiverConfig test

--- tests/test_configuration.py
+++ tests/test_configuration.py
@@ -1,15 +1,11 @@
 from barchiver.configuration import BArchiverConfig
+import os
 
 
 def test_barchiver_config():
+    os.environ["SPOTIFY_CLIENT_ID"] = "your_client_id"
+    os.environ["SPOTIFY_CLIENT_SECRET"] = "your_client_secret"
     config = BArchiverConfig()
-
-    # Test default values
-    assert config.spotify_client_id == ""
-    assert config.spotify_client_secret == ""
-
     # Test setting values
-    config.spotify_client_id = "your_client_id"
-    config.spotify_client_secret = "your_client_secret"
     assert config.spotify_client_id == "your_client_id"
     assert config.spotify_client_secret == "your_client_secret"
